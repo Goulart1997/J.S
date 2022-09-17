34002bb48aeb4a550a811d42f979b408aabfa77e
Goulart1997 <111318709+Goulart1997@users.noreply.github.com>
2022-09-16 21:51:26 -0300

    Add files via upload

--- preço.js
+++ preço.js
@@ -1,9 +1,11 @@
 const prompt = require("prompt-sync")()
 
-const nome = prompt("Nome do Produto")
-const preco = Number(prompt("Preço do produto"))
-if (preco <= 100){
-    console.log(`somente a vista`)
+const nome = prompt("Nome do Produto ")
+const preco = Number(prompt("Preço do produto "))
+const parcela = preco / 3
+if (preco < 100){
+    console.log(`${nome} será somente a vista`)
 } else {
-    console.log(`Pode pagar em 3x`)
+    console.log(`${nome} Pode ser pago em 3x de ${parcela}`)
 }
+// Ler decrição e preço de um produto .Se o preço for inferior a 100 exibir somente a visa , se nao exibir pode pagar em 3x / produto sapato preço 120
